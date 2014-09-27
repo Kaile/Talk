1c0db6a9b0f8b6e39a65452a8261162dd879cb8f
Mihail Kornilov <fix-06@yandex.ru>
2014-09-28 00:50:46 +0300

    Создание CRUD для модели пользователей.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,16 +35,15 @@ AppAsset::register($this);
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
+                    ['label' => Yii::t('app', 'Home'), 'url' => ['/site/index']],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->login . ')',
-                            'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
-            ]);
+					['label' => 'Logout (' . Yii::$app->user->identity->login . ')',
+					 'url' => ['/site/logout'],
+					 'linkOptions' => ['data-method' => 'post']],
+					['label' => Yii::t('app', 'Users'), 'url' => ['/users/index']],
+				],
+			]);
             NavBar::end();
         ?>
 
