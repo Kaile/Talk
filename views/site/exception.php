cda05e6c7a8f2dedc93a33dccb9c54ee993e8ef5
Mihail Kornilov <fix-06@yandex.ru>
2014-11-22 09:54:26 +0300

    Базовый модуль регистрации.

---
+++ views/site/exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mihail Kornilov
+ * Date: 22.11.2014
+ * Time: 10:08
+ */ 
+
+/* @var $this yii\base\View */
+?>
+
+<?php $this->title = Yii::t('app', 'Unexpected exception'); ?>
+
+<?=$message?>
--- views/site/index.php
+++ views/site/index.php
@@ -1,40 +1,17 @@
 <?php
-/* @var $this yii\web\View */
-/* @var $user app\models\Users */
-$this->title = Yii::t('app', 'The Talk');
+/**
+ * Created by PhpStorm.
+ * User: Mihail Kornilov
+ * Date: 16.11.2014
+ * Time: 0:16
+ */
+
+/* @var $this Yii\web\View */
+
+$this->title = 'Welcome to the Talk';
 ?>
-<div class="site-index">
-    <div class="body-content">
-		<div class="chat-window">
-			<div ng-controller="ReceiveTextCtrl">
-				<div class="text-output" id="outputText"></div>
-				<div id="message-window" class="message-window"></div>
-			</div>
 
-			<div class="row" ng-controller="SendTextCtrl">
-				<textarea
-					autofocus="true"
-					class="text-input"
-					ng-model="inputText"
-					ng-change="storeText()"
-				></textarea>
-                <input id="message-fix" class="button" type="button" value="<?=Yii::t('app', 'Fix')?>" ng-click="fixMessage()" />
-			</div>
-		</div>
-		<div class="contact-list">
-			<div id="contact-title">
-				<?=Yii::t('app', 'Contact list')?>
-			</div>
-			<?php foreach ($users as $user): ?>
-				<?php $user = (object) $user; ?>
-				<div contact_id="<?=$user->id?>" class="contact">
-					<span class="contact-login" title="<?=$user->registered?>" >
-						<?=$user->login?>
-					</span>
-				</div>
-			<?php endforeach ?>
-		</div>
-    </div>
-
-
+<div class="site-index center-center">
+	<h1><?=Yii::t('app', 'Welcome to the Talk messenger')?></h1>
+	<?=$this->render('register', ['registration' => $registration]);?>
 </div>
